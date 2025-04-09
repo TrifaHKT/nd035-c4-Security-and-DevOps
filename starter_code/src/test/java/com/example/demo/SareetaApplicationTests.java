3f39d9b0e99f41c0fe033ea2396d9cf5f6a6bc0d
Trifa <Trifa.KS.Cheung@pccw.com>
2025-04-09 16:35:56 +0800

    [TESTING] Add JWT related testing

--- starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
+++ starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
 
@@ -10,7 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.controllers.CartController;
@@ -20,6 +25,7 @@ import com.example.demo.controllers.UserController;
 import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.Item;
 import com.example.demo.model.requests.CreateUserRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.jupiter.api.Test;
 
@@ -31,11 +37,14 @@ import org.slf4j.LoggerFactory;
 @AutoConfigureMockMvc
 public class SareetaApplicationTests {
 
-	private static final Logger logger = LoggerFactory.getLogger(SareetaApplicationTests.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(SareetaApplicationTests.class);
 
 	private static final String TEST_USERNAME = "TestUser1";
 	private static final String TEST_PWD = "abcd@4567";
 	private static final String TEST_PWD_CONFIRM = "abcd@4567";
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Autowired
 	private CartController cartController;
@@ -178,6 +187,62 @@ public class SareetaApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
+	}
+
+	@Test
+	public void givenValidLoginAndReceivedJWT_whenRequestWithJWT_thenReturnUserInformationSuccessful() throws Exception {
+    CreateUserRequest createUserRequest = getCreateUserRequest();
+    mockMvc.perform(post("/api/user/create")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(new ObjectMapper().writeValueAsString(createUserRequest)))
+            .andExpect(status().isOk())
+						.andReturn();
+
+		//VERIFIED: get JSON successfully.
+		//LOGGER.info("The created user response is: {}", createUserResult.getResponse().getContentAsString()); 
+		
+    String loginRequest = """
+        {
+            "username": "%s",
+            "password": "%s"
+        }
+    """.formatted(TEST_USERNAME, TEST_PWD);
+
+    MvcResult result = mockMvc.perform(post("/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(loginRequest))
+            .andExpect(status().isOk())
+            .andReturn();
+
+    String token = result.getResponse().getHeader("Authorization");
+		// LOGGER.info("The token is: {}", token);
+    assertNotNull(token);
+
+    mockMvc.perform(post("/api/cart/addToCart")
+            .header("Authorization", token)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"itemId\":1,\"quantity\":1,\"username\":\"" + TEST_USERNAME + "\"}"))
+            .andExpect(status().isOk());
+	}
+
+	@Test
+	public void givenNewUserCreate_whenRequestWithoutJWT_thenReturn403() throws Exception {
+
+		CreateUserRequest createUserRequest = getCreateUserRequest();
+    mockMvc.perform(post("/api/user/create")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(new ObjectMapper().writeValueAsString(createUserRequest)))
+            .andExpect(status().isOk())
+						.andReturn();
+
+		//VERIFIED: get JSON successfully.
+		//LOGGER.info("The created user response is: {}", createUserResult.getResponse().getContentAsString()); 
+
+		mockMvc.perform(post("/api/cart/addToCart")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content("{\"itemId\":1,\"quantity\":1,\"username\":\"" + TEST_USERNAME + "\"}"))
+						.andExpect(status().isForbidden());
 
 	}
 
